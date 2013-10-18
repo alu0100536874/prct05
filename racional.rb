804549ba15a549baf58640093b9151d9a2a7daaa
Krishna Hemnani <alu0100536874@ull.edu.es>
2013-10-18 11:39:46 +0100

    1era version

---
+++ racional.rb
@@ -0,0 +1,38 @@
+# Implementar en este fichero la clase para crear objetos racionales
+
+#require "./gcd.rb"
+
+class Fraccion
+  
+  def initialize(valor)
+  @valor = Integer(valor)
+  end
+ 
+  # devuelve una cadena con la representación del racionales
+  def to_s()
+    
+  end
+  
+#   def suma
+#   end
+#   
+#   def resta 
+#   end
+#   
+#   def producto
+#   end
+#   
+#   def division
+#   end
+#   * `suma`     # devuelve un nuevo racional que suma al objeto que invoca el que le pasan como parámetro
+#   * `resta`    # devuelve un nuevo racional que resta al objeto que invoca el que le pasan como parámetro 
+#   * `producto` # devuelve un nuevo racional que multiplica al objeto que invoca el que le pasan como parámetro
+#   * `division` # devuelve un nuevo racional que multiplica al objeto que invoca el que le pasan como parámetro
+#   
+  
+end
+
+
+b = Fraccion.new(3)
+
+puts "#{@valor}"
